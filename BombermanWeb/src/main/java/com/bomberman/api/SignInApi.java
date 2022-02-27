b6b62d1ebab623294f35f66a9640607c82b2ab0e
tanguy <tanguy.jouvin@hotmail.com>
2022-02-27 16:47:06 +0100

    add API

---
+++ BombermanWeb/src/main/java/com/bomberman/api/SignInApi.java
@@ -0,0 +1,44 @@
+package com.bomberman.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class SignInApi
+ */
+public class SignInApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignInApi() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("--------------------------------");
+        System.out.println(username + " : " + password);
+
+        PrintWriter output = new PrintWriter(response.getOutputStream(), true);
+        output.println("success");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
